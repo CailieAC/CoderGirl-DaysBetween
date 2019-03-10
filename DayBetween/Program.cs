919516491856c463ad99c6fcb0cbc037b0d320b0
Carlile Upfront <carlileupfront@users.noreply.github.com>
2019-03-09 23:37:35 -0600

    commit1

--- DayBetween/Program.cs
+++ DayBetween/Program.cs
@@ -7,10 +7,26 @@ namespace DaysBetween
         public static void Main()
         {
             // TODO:  Get two dates from the user.
+            Console.Write("Enter first date: ");
+            DateTime firstDate = DateTime.Parse(Console.ReadLine());
 
+            Console.Write("Enter second date: ");
+            DateTime secondDate = DateTime.Parse(Console.ReadLine());
+
+            double numberOfDaysBetween = DaysBetween(firstDate, secondDate);
+
+            Console.WriteLine(numberOfDaysBetween);
             Console.ReadLine();
         }
 
         // TODO: Create that has a method that counts the number of days between two different dates.
+        public static double DaysBetween(DateTime firstDate, DateTime secondDate)
+        {
+            TimeSpan numberOfDaysBetween = secondDate.Subtract(firstDate);
+            double timeSpan = numberOfDaysBetween.TotalDays;
+            if (timeSpan < 0)
+                timeSpan = timeSpan * -1;
+            return timeSpan;
+        }
     }
 }
